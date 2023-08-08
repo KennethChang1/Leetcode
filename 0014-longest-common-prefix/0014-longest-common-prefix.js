8e4bc3d11f13f97f60d801fbdb77725c00dd4598
KennethChang1 <100064461+KennethChang1@users.noreply.github.com>
2023-08-08 15:50:21 +0700

    Time: 47 ms (96.85%), Space: 42.3 MB (62.12%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+   let prefix = "";
+    
+    for (let i = 0; i < strs[0].length; i++) {
+        let char = strs[0][i];
+        
+        if (strs.every(str => str[i] === char)) {
+            prefix += char;
+        } else {
+            break;
+        }
+    }
+    
+    return prefix;
+};
