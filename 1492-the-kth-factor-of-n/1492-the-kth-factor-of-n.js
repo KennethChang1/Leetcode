213d6465cb5a6d202cb76673929062a92ad256a4
KennethChang1 <100064461+KennethChang1@users.noreply.github.com>
2023-10-19 13:40:46 +0700

    Time: 59 ms (22.77%), Space: 42.5 MB (21.88%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var kthFactor = function(n, k) {
+    let factors = [];
+    for (let i = 1; i <=n; i++) {
+        if (n % i === 0) {
+            factors.push(i);
+        }
+        
+        if (factors.length === k) {
+            return factors.at(-1)
+        }
+    }
+    
+    
+    return -1
+    console.log(factors)
+};
