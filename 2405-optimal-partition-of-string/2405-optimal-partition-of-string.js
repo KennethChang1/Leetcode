e0bb10d0fda83bb1b45e7e6002116cd2fd5f5918
KennethChang1 <100064461+KennethChang1@users.noreply.github.com>
2023-10-19 14:08:46 +0700

    Time: 74 ms (92.32%), Space: 49.3 MB (70.97%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.js
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.js
@@ -3,18 +3,30 @@
  * @return {number}
  */
 var partitionString = function(s) {
-    let curr = "";
-    let count = 0;
-    
-    for (let i = 0; i < s.length; i++) {
-        if (curr.indexOf(s[i]) != -1) {
-            curr = "";
-            count = count + 1;
-        }
-        
-        curr += s[i];
-        console.log(curr)
+    let count = 1;
+    let prevChars = s[0];
+
+    for (let i = 1; i < s.length; i++) {
+      if (!prevChars.includes(s[i])) {
+        prevChars += s[i];
+      } else {
+        count++;
+        prevChars = s[i];
+      }
     }
+
+    return count;
+//     let curr = "";
+//     let count = 0;
     
-    return count + 1
+//     for (let i = 0; i < s.length; i++) {
+//         if (curr.indexOf(s[i]) != -1) {
+//             curr = "";
+//             count = count + 1;
+//         }
+        
+//         curr += s[i];
+//     }
+    
+//     return count + 1
 };
