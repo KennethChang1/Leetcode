c0fb876b100d10d63aa5dfc62b5b65b5c0a7d741
KennethChang1 <100064461+KennethChang1@users.noreply.github.com>
2023-12-15 10:20:26 +0700

    Time: 66 ms (26.97%), Space: 43 MB (98.95%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let count = 1;
+    let curr = null;
+    for (let i = 0; i < nums.length; i++) {
+        if (curr === nums[i]) {
+            count = count + 1;
+        } else {
+            count = 1;
+        }
+        curr = nums[i];
+        
+        if (count > 2) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    
+    return nums.length
+};
+
