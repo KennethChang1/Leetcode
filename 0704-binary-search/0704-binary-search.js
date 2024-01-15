083d7e70e6e584d759db6ded23b5a698240cab0a
KennethChang1 <100064461+KennethChang1@users.noreply.github.com>
2024-01-15 10:59:21 +0700

    Time: 58 ms (47.28%), Space: 44.9 MB (55.86%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,19 +4,22 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
+    let l = 0;
+    let r = nums.length - 1;
     
-    while (left <= right) {
-        const mid = (left + right) >> 1;
-
-        if (nums[mid] === target) return mid;
+    while (l <= r) {
+        let mid = Math.floor((l + r)/2);
         
-        if (nums[mid] > target) {
-            right = mid - 1;
-        } else if (nums[mid] < target) {
-            left = mid + 1;
+        if (nums[mid] === target) {
+            return mid;
         }
+        
+        if (nums[mid] < target) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+        
     }
     
     return -1
